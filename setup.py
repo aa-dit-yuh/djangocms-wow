057dd21afb7a903877494bd094923d6b1168bd47
Aditya Narayan <narayanaditya95@gmail.com>
2015-05-03 02:03:39 +0530

    Bump status to Beta

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ INSTALL_REQUIRES = [
 ]
 
 CLASSIFIERS = [
-    'Development Status :: 3 - Alpha',
+    'Development Status :: 4 - Beta',
     'Environment :: Web Environment',
     'Framework :: Django',
     'Intended Audience :: Developers',
